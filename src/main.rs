0673aa18c111311fe94d9fa6204d295a0d74e507
rm-21 <mittalrishabh181@gmail.com>
2023-08-26 08:20:53 +0530

    Health check + CI update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use newsletter::configurations::get_configuration;
+use newsletter::startup::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    run(
+        TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port))
+            .expect("Failed to bind port 8000"),
+    )?
+    .await
 }
---
+++ src/routes/health_check.rs
@@ -0,0 +1,7 @@
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer};
+use std::net::TcpListener;
+
+pub async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
